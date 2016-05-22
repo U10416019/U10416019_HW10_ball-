53f103f7976d9330bdc13ca878b05bb8da2d6372
WenChi <u10416019@go.utaipei.edu.tw>
2016-05-22 20:56:55 +0800

    Main class
    This class is the main class of the PaneBall and BounceBallControl.

---
+++ TestBall.java
@@ -0,0 +1,9 @@
+//U10416019鄭文琪
+
+import javafx.application.Application;
+
+public class TestBall{
+	public static void main(String[] args){
+		Application.launch(BounceBallControl.class, args);
+	}
+}
